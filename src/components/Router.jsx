5a204d2300be56d3eb22fe5dc1cb807bb1231036
Bulmond <lfilipe.mottaa@gmail.com>
2025-07-28 09:22:46 +0200

    fixed initial commit

---
+++ src/components/Router.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home.jsx";
+import About from "../pages/About.jsx";
+import Rentals from "../pages/Rentals.jsx";
+import Error from "../pages/Error.jsx";
+import Header from "../components/Header.jsx";
+import Footer from "./Footer.jsx";
+
+const Router = () => {
+    return (
+        <BrowserRouter
+            future={{
+                v7_startTransition: true,
+                v7_relativeSplatPath: true,
+            }}
+        >
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/rental/:currentRental" element={<Rentals />} />
+                <Route path="*" element={<Error />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    );
+};
+
+export default Router;
---
+++ src/pages/Rentals.jsx
@@ -0,0 +1,9 @@
+const Rentals = () => {
+    return (
+        <div>
+            <h1>Template</h1>
+        </div>
+    );
+};
+
+export default Rentals;
